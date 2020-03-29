983f4864496a5e6477f950163f66c6b04d458275
Latricia <tricianickelberry@gmail.com>
2020-03-29 15:47:50 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
+  # validates_presence_of :name, :email, :password
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
+  end
 end
+
+  
